1233eff3cf66d3d45cf1ff9b765de50fa9cef928
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-03-20 10:41:24 +0000

    changed font colour from black to white

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-const colours = ['green', 'red', 'rgba(133,122,200', '#f15025'];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
-const btn = document.getElementById('btn');
-const colour = document.querySelector('.colour');
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
 
-btn.addEventListener('click', function() {
-    console.log(document.body);
-    // get random number between 0 - 3 
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber)
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
 
-    document.body.style.backgroundColor = colours[randomNumber];
-    colour.textContent = colours[randomNumber];
-})
-
-function getRandomNumber(){
-    return Math.floor(Math.random()*colours.length);
-    
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
 }
